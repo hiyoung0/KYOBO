554b7b07121d6c8f8c2a0eca3f53a64c8e63376e
hiyoung0 <104354817+hiyoung0@users.noreply.github.com>
2022-07-22 13:18:39 +0900

    project05

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//.tap_wellcome_kyobo
 $.ajax({
     method: "GET",
     url: "https://dapi.kakao.com/v3/search/book?target=title",
@@ -10,11 +11,6 @@ $.ajax({
         $(".ajax_title").append(msg.documents[1].title);
         $(".cn_author").append(msg.documents[1].authors[0]);
         $(".cn_publisher").append(msg.documents[1].publisher);
-
-        // var str = msg.documents[0].contents;
-        // var str2 = str.substring(0, 60);
-
-        // $(".box").append("<p>" + str2 + "</p>");
 
     });
 
@@ -29,9 +25,6 @@ $.ajax({
     .done(function (msg) {
 
         $(".book_2").append("<a href='#'>" + "<img src='" + msg.documents[0].thumbnail + "'/>" + "</a>");
-        // $(".ajax_title").append(msg.documents[1].title);
-        // $(".cn_author").append(msg.documents[1].authors[0]);
-        // $(".cn_publisher").append(msg.documents[1].publisher);
 
     });
 
@@ -46,8 +39,61 @@ $.ajax({
     .done(function (msg) {
 
         $(".book_3").append("<a href='#'>" + "<img src='" + msg.documents[0].thumbnail + "'/>" + "</a>");
-        // $(".ajax_title").append(msg.documents[1].title);
-        // $(".cn_author").append(msg.documents[1].authors[0]);
-        // $(".cn_publisher").append(msg.documents[1].publisher);
+    });
+
+
+
+
+//.new_book_kblist1
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "비올레트" },
+    headers: { Authorization: "KakaoAK 9a32b66e83b5960bf00283d931393441" }
+})
+    .done(function (msg) {
+
+        $(".new_book_box1").append("<img src='" + msg.documents[0].thumbnail + "'/>");
+        $(".kblist_title1").append(msg.documents[0].title);
 
     });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "조우성 변호사" },
+    headers: { Authorization: "KakaoAK 9a32b66e83b5960bf00283d931393441" }
+})
+    .done(function (msg) {
+
+        $(".new_book_box2").append("<img src='" + msg.documents[0].thumbnail + "'/>");
+        $(".kblist_title2").append(msg.documents[0].title);
+
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "우는 법을" },
+    headers: { Authorization: "KakaoAK 9a32b66e83b5960bf00283d931393441" }
+})
+    .done(function (msg) {
+
+        $(".new_book_box3").append("<img src='" + msg.documents[0].thumbnail + "'/>");
+        $(".kblist_title3").append(msg.documents[0].title);
+
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "지배한 단어들" },
+    headers: { Authorization: "KakaoAK 9a32b66e83b5960bf00283d931393441" }
+})
+    .done(function (msg) {
+
+        $(".new_book_box4").append("<img src='" + msg.documents[0].thumbnail + "'/>");
+        $(".kblist_title4").append(msg.documents[0].title);
+
+    });
+
