2c9daef058981a99e8f63d442a3163b3247fe681
hiyoung0 <104354817+hiyoung0@users.noreply.github.com>
2022-07-26 14:14:23 +0900

    project05

--- js/index.js
+++ js/index.js
@@ -1,7 +1,44 @@
 $(document).ready(function(){
+    //bg_wellcome_1
+    $(".category1").hover(function(){
+        $(".mainEvent1").show();
+        $(".mainEvent2").hide();
+        $(".bg_wellcome_1").css("background-color","rgb(230, 233, 246)");
+        $(".category1").addClass("typeclass");
+        $(".category2").removeClass("typeclass");
+    });
+    $(".category2").hover(function(){
+        $(".mainEvent1").hide();
+        $(".mainEvent2").show();
+        $(".bg_wellcome_1").css("background-color","rgb(161, 221, 192)");
+        $(".category2").addClass("typeclass");
+        $(".category1").removeClass("category1");
+    });
+
+
     //bg_wellcome_8
     $(".rising_list").hover(function(){
-        $(".rising_btn_prev").show();
-        $(".rising_btn_next").show();
+        $(".rising_btn_prev").css("display","block");
+        $(".rising_btn_next").css("display","block");
+    }, function(){
+        $(".rising_btn_prev").css("display","none");
+        $(".rising_btn_next").css("display","none");
     });
+
+    $(".rising_btn_prev").hover(function(){
+        $(".rising_btn_prev").css("display","block");
+        $(".rising_btn_next").css("display","block");
+    },function(){
+        $(".rising_btn_prev").css("display","none");
+        $(".rising_btn_next").css("display","none");
+    });
+    $(".rising_btn_next").hover(function(){
+        $(".rising_btn_prev").css("display","block");
+        $(".rising_btn_next").css("display","block");
+    }, function(){
+        $(".rising_btn_prev").css("display","none");
+        $(".rising_btn_next").css("display","none");
+    });
+
+    //
 });
